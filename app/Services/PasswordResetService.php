3de97b1a0bdbe737f0f52c8b9716c16bf3442591
niru <codeinmindinfotech@gmail.com>
2025-09-15 19:09:02 -0600

    env local only sent email

--- app/Services/PasswordResetService.php
+++ app/Services/PasswordResetService.php
@@ -2,24 +2,27 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Password;
 
 class PasswordResetService
 {
     public function sendResetLink($user, string $type, string $brokerName)
     {
-        $brokerManager = app('auth.password');
-        $brokerManager->setCompanyId($user->company_id ?? null);
-        $brokerManager->setType($type);
+        $status = Password::RESET_LINK_SENT;
+        if (App::environment('local')) {
+            $brokerManager = app('auth.password');
+            $brokerManager->setCompanyId($user->company_id ?? null);
+            $brokerManager->setType($type);
 
-        $broker = $brokerManager->broker($brokerName);
+            $broker = $brokerManager->broker($brokerName);
 
-        $status = $broker->sendResetLink(['email' => $user->email]);
+            $status = $broker->sendResetLink(['email' => $user->email]);
 
-        if ($status !== Password::RESET_LINK_SENT) {
-            throw new \Exception("Failed to send reset link. Status: $status");
+            if ($status !== Password::RESET_LINK_SENT) {
+                throw new \Exception("Failed to send reset link. Status: $status");
+            }
         }
-
         return $status;
     }
 }
