e3e378cb9ccf8ad515aad1ce2eb41b65b6d91312
niru <codeinmindinfotech@gmail.com>
2025-11-06 18:38:27 -0700

    add url

--- config/onlyoffice.php
+++ config/onlyoffice.php
@@ -2,5 +2,5 @@
 
 return [
     'server_url' => env('ONLYOFFICE_DOC_SERVER', 'http://localhost:8080'),
-    'jwt_secret' => env('ONLYOFFICE_JWT_SECRET', '7Lz9DU7AJaSm8E0Hwsa8WHKddU0TcFVd'),
+    'jwt_secret' => env('ONLYOFFICE_JWT_SECRET', 'es41yTexoVsdyf75n9mK'),
 ];
